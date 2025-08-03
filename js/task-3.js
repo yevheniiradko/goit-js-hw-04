867e53c7b046c86b97951047064c190c40407059
Yevhenii Radko <yevhenii.radko@gmail.com>
2025-08-03 12:16:56 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function filterArray(numbers, value) {
+  const sumArray = [];
+
+  for (const item of numbers) {
+    if (item > value) {
+      sumArray.push(item);
+    }
+  }
+  return sumArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
