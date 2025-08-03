c2050f61fdfa98097b39a47d6f47ec42c740df43
Yevhenii Radko <yevhenii.radko@gmail.com>
2025-08-03 13:57:26 +0300

    first commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,20 @@
-function slugify(title) {
-  const titleToLowerCase = title.toLowerCase();
-  const words = titleToLowerCase.split(' ');
-  const slug = words.join('-');
+function isEnoughCapacity(products, containerSize) {
+  let total = 0;
 
-  return slug;
+  for (let key in products) {
+    total += products[key];
+  }
+  if (total <= containerSize) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
